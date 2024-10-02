6335df42d2a1a42612588cbe8f93463669a9d85a
Nicole <150757244+CatGatoCat@users.noreply.github.com>
2024-10-02 16:26:57 -0400

    huh

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList list = new ArrayList<>();
+
+        System.out.println("I hate Java now");
+
+    }
+    
+    
+}

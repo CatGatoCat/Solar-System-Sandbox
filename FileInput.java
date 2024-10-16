bd56f348c0c9d53262b2af1d7113197fd15b73f6
Nicole <150757244+CatGatoCat@users.noreply.github.com>
2024-10-16 16:10:33 -0400

    :)

--- FileInput.java
+++ FileInput.java
@@ -1,5 +1,9 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
+
+
 
 /*Req. 4: The system will save all manually input data to a file and copy data to an array. 
 
@@ -13,30 +17,48 @@ Req. 4.4: For each planet the software should ask the user to input a name, mass
 
 Req. 4.5: The system shall create an array with the user’s input data  */
 
+//private String filePath = "data.txt"; 
 
 
 public class FileInput {
 
-
-    private ArrayList<Double> SimData = new ArrayList<Double>();
-
     public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
         ArrayList list = new ArrayList<>();
 
         System.out.println("What is the Solar System model?");
 
-        String Model_Name = scan.nextLine();
 
        // System.out.println(Model_Name);
 
     }
 
-    private void readInput(){
-        
+    //read information from the file and put them in the arrayList
+	private ArrayList<String> readFromFile(String filename) 
+	{
+		//ArrayList<String> data = new ArrayList<>();
+        //ArrayList<Double> SimData = new ArrayList<>();
 
-    }
+
+        ArrayList<Object> SimData = new ArrayList<>();
+
+
+		try (Scanner scan = new Scanner(new File("input.txt"))){
+			File file = new File(filename);
+
+			while(scan.hasNext()) {
+				String line = scan.nextLine().trim();
+				//data.add(line);
+			}
+
+			scan.close();
+
+		} catch(Exception e) {
+			System.out.println("Exception occured while reading from file.");
+			e.printStackTrace();
+		}
+		//return data;
+
+	}
 
     private void inputInArray(){
         

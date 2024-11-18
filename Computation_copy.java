ab425603e84cda7fcdd41e7269bf9598730c8e4a
Nicole <150757244+CatGatoCat@users.noreply.github.com>
2024-11-18 15:33:59 -0500

    Langrange Points 1 2 3

--- Computation_copy.java
+++ Computation_copy.java
@@ -15,6 +15,7 @@
 	
 */
 
+import static java.lang.Math.cbrt;
 import java.util.*;
 
 public class Computation_copy {
@@ -56,6 +57,22 @@ public class Computation_copy {
 		
 		return ri + "_" + ro;
 	}
+
+	private double LagrangePoint1and2(double m1, double m2, double d){
+
+		double mew = m2/(m1+m2);
+		double LangPoint1 = d*(cbrt((mew/3)));
+
+		return LangPoint1;
+	}
+
+	private double LagrangePoint3(double m1, double m2, double d){
+
+		double mew = m2/(m1+m2);
+		double LangPoint3 = (7*d/12)*mew;
+
+		return LangPoint3;
+	}
 	
 	
 	
@@ -65,9 +82,13 @@ public class Computation_copy {
 		
 		double GravitationalPull = obj.GravitationalPull(5.972*Math.pow(10, 24), 1, 6371000);
         String HabitableZone = obj.HabitableZone(1, 1, 1, 1, 1, 1);
+		double LagrangePoint1and2 = obj.LagrangePoint1and2(23445, 345556, 213993);
+		double LagrangePoint3 = obj.LagrangePoint3(343445, 3455, 334);
 		
 		System.out.println("Gravitational Pull: " + GravitationalPull);
-        System.out.println(HabitableZone);
+        System.out.println("Habitable Zone: " + HabitableZone);
+		System.out.println("Langrange Points 1 & 2: " + LagrangePoint1and2);
+		System.out.println("Langrange Point 3: " + LagrangePoint3);
 		
 	}
 
